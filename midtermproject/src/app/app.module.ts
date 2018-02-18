32e0e73e1136f683f71db10a0e4fc5a25b9bd267
mrgamon <mrgamon@hotmail.com>
2018-02-18 18:31:20 +0800

    added product component

--- midtermproject/src/app/app.module.ts
+++ midtermproject/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+import { ProductComponent } from './product/product.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
--- midtermproject/src/environments/environment.ts
+++ midtermproject/src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB-833_wI6Ha6ucYhMynmU7Uu-LsKRT43E",
+    authDomain: "firestore-crud2.firebaseapp.com",
+    databaseURL: "https://firestore-crud2.firebaseio.com",
+    projectId: "firestore-crud2",
+    storageBucket: "firestore-crud2.appspot.com",
+    messagingSenderId: "1009297380603"
+
+  }
 };
