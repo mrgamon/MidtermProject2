a7d2048e7cf10f3916e57b6968bd3b0906719091
mrgamon <mrgamon@hotmail.com>
2018-02-18 19:30:17 +0800

    integrating product to firebase

--- MidtermProject/src/app/products/shared/product.service.ts
+++ MidtermProject/src/app/products/shared/product.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Product } from './product.model';
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+    productList: AngularFireList<any>;
+    selectedProduct: Product = new Product();
+    constructor(private firebase :AngularFireDatabase ) { }
 
+    getData(){
+      this.productList = this.firebase.list('products');
+      return this.productList;
+    }
+
+    insertProduct(product : Product){
+
+      this.productList.push({
+        name: product.name,
+        description: product.description,
+        category: product.category,
+        price: product.price
+
+      });
+    }
 }
